5353133fa71e21013c90d7fac28b453356075771
AtanasBGeorgiev <atanas.b.georgiev@gmail.com>
2025-08-27 14:25:38 +0300

    Add files via upload

--- server/src/middleware/rateLimiter.ts
+++ server/src/middleware/rateLimiter.ts
@@ -1,9 +1,18 @@
 import { rateLimit } from 'express-rate-limit';
 
+//rateLimit - counts requests from each IP address
+
 const loginLimiter = rateLimit({
-    windowMs: 1 * 60 * 1000,//15 minutes 
-    max: 5,//5 attempts per 15 minutes
-    message: { errorKey: 'errors.tooManyAttempts' }
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 5, // 5 attempts per 15 minutes
+    message: { errorKey: 'errors.tooManyAttempts' },
+    keyGenerator: (req) => {
+        // Use username from request body or IP address if username is not provided
+        return req.body.username || req.ip;
+    },
+    skipSuccessfulRequests: true, // Reset counter on successful login
+    standardHeaders: true,//Adds standart HTTP headers to show rate limit info in responses
+    legacyHeaders: false,//Disables legacy headers(old HTTP format with X- prefix)
 });
 
 export default loginLimiter;
